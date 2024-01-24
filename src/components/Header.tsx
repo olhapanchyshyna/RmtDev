15955ef7475a2c6e400bbf7022abb1317024d7ac
Olya <olapancisina1@gmail.com>
2024-01-24 11:01:24 +0100

    add components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,13 @@
+import BookmarksButton from './BookmarksButton'
+import Logo from './Logo'
+
 export default function Header() {
   return (
     <header className="header">
-      <div className="header__top"></div>
+      <div className="header__top">
+        <Logo/>
+        <BookmarksButton/>
+      </div>
     </header>
   );
 }
