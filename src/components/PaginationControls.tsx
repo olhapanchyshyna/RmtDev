792a7e4ebddc47885e6436e99df125e06bde90bd
Olya <olapancisina1@gmail.com>
2024-01-30 13:23:14 +0100

    made custom functions for pagination buttons + added limit for switching below 0

--- src/components/PaginationControls.tsx
+++ src/components/PaginationControls.tsx
@@ -4,6 +4,11 @@ type PaginationControlsProps = {
 	onChangePage: (direction: 'next' | 'prev') => void
 	currentPage: number
 }
+type PaginationButtonProps = {
+	onChangePage: (direction: 'next' | 'prev') => void
+	currentPage: number
+	direction: 'next' | 'prev'
+}
 
 export default function PaginationControls({
 	onChangePage,
@@ -11,24 +16,47 @@ export default function PaginationControls({
 }: PaginationControlsProps) {
 	return (
 		<section className='pagination'>
-			<button
-				className='pagination__button'
-				onClick={() => {
-					onChangePage('prev')
-				}}
-			>
-				<ArrowLeftIcon />
-				Page {currentPage - 1 }
-			</button>
-			<button
-				className='pagination__button'
-				onClick={() => {
-					onChangePage('next')
-				}}
-			>
-				Page {currentPage + 1}
-				<ArrowRightIcon />
-			</button>
+      {currentPage > 1 ? <PaginationButton
+				onChangePage={onChangePage}
+				currentPage={currentPage}
+				direction='prev'
+			/> : <button/>}
+			
+			<PaginationButton
+				onChangePage={onChangePage}
+				currentPage={currentPage}
+				direction='next'
+			/>
 		</section>
+	)
+}
+
+function PaginationButton({
+	onChangePage,
+	currentPage,
+	direction,
+}: PaginationButtonProps) {
+	return (
+		<button
+			className='pagination__button'
+			onClick={() => {
+				onChangePage(direction)
+			}}
+		>
+			{direction === 'prev' ? (
+				<>
+					<ArrowLeftIcon />
+					Page {currentPage - 1}
+				</>
+			) : null}
+
+			{direction === 'next' ? (
+				<>
+					Page {currentPage + 1}
+					<ArrowRightIcon />
+				</>
+			) : null}
+
+		</button>
 	)
 }
